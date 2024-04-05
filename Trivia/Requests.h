670a3708b8a148d7da6d9257a1718d4a38e00c4a
Shon Gutman <gshon2007@gmail.com>
2024-04-05 14:41:48 +0300

    added class Requests - contains all defenitions of requests structs

---
+++ Trivia/Requests.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <vector>
+#include <ctime>
+
+#include "IRequestHandler.h"
+
+using std::tm;
+
+#define BYTE char
+typedef std::vector<BYTE> Buffer;
+
+typedef enum RequestId
+{
+	LOGIN_REQUEST_ID = 1,
+	SIGN_UP_REQUEST_ID = 2,
+	ERROR_REQUEST_ID = 99
+}RequestId;
+
+//general info of a request 
+struct RequestInfo
+{
+	RequestId id;
+	tm recivalTime;
+	Buffer buffer;
+};
+
+//general result of request
+struct RequestResult
+{
+	Buffer response;
+	IRequestHandler* newHandler;
+};
+
+
+struct LoginRequest
+{
+	std::string username;
+	std::string password;
+};
+
+struct SignupRequest
+{
+	std::string username;
+	std::string password;
+	std::string email;
+};
