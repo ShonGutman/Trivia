0cfa686967c69e5abcf42d5a27a9997ff5b56170
Shon Gutman <gshon2007@gmail.com>
2024-04-05 15:48:43 +0300

    finished isRequestRelevant

--- Trivia/IRequestHandler.h
+++ Trivia/IRequestHandler.h
@@ -11,6 +11,21 @@ struct RequestResult;
 
 class IRequestHandler
 {
+	/*
+	 * Checks if the provided RequestInfo object is relevant to this request handler.
+	 * This function is used to determine if the handler should process the request
+	 * or not.
+	 *
+	 * @param info - The RequestInfo object containing information about the request.
+	 * @return true if the request is relevant to this handler, false otherwise.
+	 */
 	virtual bool isRequestRelevant(RequestInfo info) = 0;
+
+	/*
+	 * Handles the incoming request and produces a result based on the provided information.
+	 *
+	 * @param info The information related to the request.
+	 * @return The result of handling the request.
+	 */
 	virtual RequestResult handleRequest(RequestInfo info) = 0;
 };
---
+++ Trivia/LoginRequestHandler.cpp
@@ -0,0 +1,13 @@
+#include "LoginRequestHandler.h"
+
+bool LoginRequestHandler::isRequestRelevant(RequestInfo info)
+{
+    return info.id == LOGIN_REQUEST_ID || info.id == SIGN_UP_REQUEST_ID;
+}
+
+RequestResult LoginRequestHandler::handleRequest(RequestInfo info)
+{
+    //not implemented yet!
+    return RequestResult();
+}
+
