b3712bbf7e635869aa1c5a4b142df674e32e5fd4
Shelly Slavutin <shelly.slavutin@gmail.com>
2024-04-09 16:29:33 +0300

    Made it to a true while loop and added supporting in error msg

--- Trivia/demo-mt-client.py
+++ Trivia/demo-mt-client.py
@@ -17,51 +17,52 @@ def connect_to_server():
     return server_sock
 
 def main():
-    try:
-        # Attempt to connect to the server
-        server_sock = connect_to_server()
-        print("Connection successful")
-
+    while True:
         try:
-            message_type = input("Type 'login' or 'signup': ").lower()
+            # Attempt to connect to the server
+            server_sock = connect_to_server()
+            print("Connection successful")
 
-            if message_type == 'login':
-                # Prepare login message in JSON format
-                username = input("Enter your username: ")
-                password = input("Enter your password: ")
-                message_type = 1;
-                message_data = {"username": username, "password": password}
-                
-            elif message_type == 'signup':
-                # Prepare signup message in JSON format
-                username = input("Enter your username: ")
-                password = input("Enter your password: ")
-                mail = input("Enter your email: ")
-                message_type = 2;
+            try:
+                message_type = input("Type 'login' or 'signup': ").lower()
 
-                message_data = {"username": username, "password": password, "mail": mail}
-            else:
-                print("Invalid message type")
-                return
+                if message_type == 'login':
+                    # Prepare login message in JSON format
+                    username = input("Enter your username: ")
+                    password = input("Enter your password: ")
+                    message_type = 1
+                    message_data = {"username": username, "password": password}
+                    
+                elif message_type == 'signup':
+                    # Prepare signup message in JSON format
+                    username = input("Enter your username: ")
+                    password = input("Enter your password: ")
+                    mail = input("Enter your email: ")
+                    message_type = 2
 
-            # Add code and message length according to the format
-            data = []
-            data.append(message_type)
-            data.append(len(json.dumps(message_data)))
-            data.extend(json.dumps(message_data))
+                    message_data = {"username": username, "password": password, "mail": mail}
+                else:
+                    message_type = 99
+                    message_data = {}
 
+                # Add code and message length according to the format
+                data = []
+                data.append(message_type)
+                data.append(len(json.dumps(message_data)))
+                data.append(json.dumps(message_data))
 
-            # Send message to server in JSON format
-            server_sock.sendall(data.encode())
+                # Send message to server in JSON format
+                server_sock.sendall(json.dumps(data).encode())
 
-            # Receive response from server
-            server_data = server_sock.recv(1024)
-            print("Received from server:", server_data.decode())
-        finally:
-            # Close the connection
-            server_sock.close()
-    except Exception as e:
-        print("Could not connect:", e)
+                # Receive response from server
+                server_data = server_sock.recv(1024)
+                print("Received from server:", server_data.decode())
+            finally:
+                # Close the connection
+                server_sock.close()
+        except Exception as e:
+            print("Could not connect:", e)
+            break;
 
 if __name__ == "__main__":
     main()
