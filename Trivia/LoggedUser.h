504d9d1f9df59b799140b923fbbf8470dba9af5e
Shon Gutman <gshon2007@gmail.com>
2024-04-12 18:37:15 +0300

    here you go shelly

--- Trivia/LoggedUser.h
+++ Trivia/LoggedUser.h
@@ -9,9 +9,23 @@ public:
 	// CTOR //
 	LoggedUser(const std::string& username);
 
-	// get and set //
+
+	/**
+	 * Gets the name of the LoggedUser.
+	 *
+	 * This function returns the username associated with the LoggedUser object.
+	 *
+	 * @return The username of the LoggedUser.
+	 */
 	std::string getName() const;
 
+	/**
+	 * Sets the name of the LoggedUser.
+	 *
+	 * This function sets the username associated with the LoggedUser object.
+	 *
+	 * @param username The username to set.
+	 */
 	void setName(const std::string& username);
 
 	//add operator overloading of <, <=, ==, !=, >=, and > to support std::set
