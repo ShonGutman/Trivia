d9bc7748406dbf6bd6cbacf2c34c48ca3ef68a5d
Shelly Slavutin <shelly.slavutin@gmail.com>
2024-04-14 17:03:55 +0300

    fixed

--- Trivia/JsonResponsePacketSerializer.h
+++ Trivia/JsonResponsePacketSerializer.h
@@ -11,9 +11,6 @@ using json = nlohmann::json;
 #define SIZE_BYTE 8
 #define MSG_MAX_SIZE 4
 
-#define STATUS_KEY "status"
-#define MESSAGE_KEY "message"
-
 class JsonResponsePacketSerializer
 {
 public:
