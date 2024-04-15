9663abfc3fe7a89cc6c656e6035746e0edf7746a
Shon Gutman <gshon2007@gmail.com>
2024-04-15 21:58:09 +0300

    added check for address + comments

--- Trivia/RegexHelper.cpp
+++ Trivia/RegexHelper.cpp
@@ -12,7 +12,19 @@ bool RegexHelper::isPasswordLegal(const std::string& password)
 
 bool RegexHelper::isEmailLegal(const std::string& email)
 {
-	const std::regex expression("^[\da-zA-Z]+@[\da-zA-Z]+(\.[a-z]+)+$");
+	//email looks like this:
+	//start@domain.extension.again
+	const std::regex expression("^[\da-zA-Z]+@[a-zA-Z]+(\.[a-z]+)+$");
 
 	return std::regex_match(email, expression);
+}
+
+bool RegexHelper::isAddressLegal(const std::string& address)
+{
+	//address is like this:
+	// Street (space) Apt (space) City
+	//credit: https://stackoverflow.com/questions/24744819/does-stdregex-support-i-for-case-insensitivity
+	const std::regex expression("^[a-z]+\s[\d]+\s[a-z]+$", std::regex_constants::icase);
+
+	return std::regex_match(address, expression);
 }
--- Trivia/RegexHelper.h
+++ Trivia/RegexHelper.h
@@ -6,8 +6,46 @@
 class RegexHelper
 {
 public:
+
+	/**
+	 * Checks if a password meets the specified criteria.
+	 *
+	 * This function checks if a password meets the following criteria:
+	 * - Contains at least one number
+	 * - Contains at least one lowercase letter
+	 * - Contains at least one uppercase letter
+	 * - Contains at least one special character from the set [@!#$%^&*]
+	 * - Has a length of at least 8 characters
+	 *
+	 * @param password The password to be validated.
+	 * @return true if the password meets the criteria, false otherwise.
+	 */
 	static bool isPasswordLegal(const std::string& password);
 
+	/**
+	 * Checks if an email address is in a valid format.
+	 *
+	 * This function checks if an email address is in a valid format, adhering to the
+	 * standard email address structure:
+	 * - The local part can contain alphanumeric characters.
+	 * - The domain part must contain at least one dot.
+	 * - The domain extension can contain only lowercase alphabets.
+	 *
+	 * @param email The email address to be validated.
+	 * @return true if the email address is in a valid format, false otherwise.
+	 */
 	static bool isEmailLegal(const std::string& email);
 
+	/**
+	 * Checks if an address is in a valid format.
+	 *
+	 * This function checks if an address is in a valid format, adhering to the standard structure:
+	 * - The address consists of a street name, apartment number, and city, separated by spaces.
+	 * - The street name and city can contain only alphabets.
+	 * - The apartment number can contain only digits.
+	 *
+	 * @param address The address to be validated.
+	 * @return true if the address is in a valid format, false otherwise.
+	 */
+	static bool isAddressLegal(const std::string& address);
 };
