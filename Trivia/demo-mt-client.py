8b61680613c32cdf97b2f219f7cb2f9830203112
Shon Gutman <gshon2007@gmail.com>
2024-04-16 16:42:08 +0300

    FIXED THE PYTHON GODDDDDDDDDD I HATE PYTHON SO BADLY

--- Trivia/demo-mt-client.py
+++ Trivia/demo-mt-client.py
@@ -31,21 +31,18 @@ def connect_to_server():
 
 def convert_to_byte_string(num):
     """
-    Convert a number to a byte string representation and reverse it.
+    Convert a number to 4 bytes representation and reverse it.
 
     Note: This function assumes that the input number can be represented
     as a 4-byte unsigned integer in little-endian byte order.
 
     :param num: The number to be converted to a byte string.
     :type num: int
-    :return: The reversed byte string representation of the input number.
-    :rtype: str
+    :return: The reversed byte representation of the input number.
+    :rtype: bytes
     """
 
-    bytesBuffer = struct.pack("I", num)  # Pack the number into bytes
-
-    # need to reverse string
-    return bytesBuffer.decode('utf-8')[::-1]
+    return struct.pack("I", num)[::-1]
 
 
 def main():
@@ -78,33 +75,33 @@ def main():
                 address = input("Enter your address: ")
                 phoneNumber = input("Enter your phone number: ")
                 birthday = input("Enter your birthday: ")
-                
+
                 message_type = SIGN_UP_CODE
-                
+
                 message_data = {"username": username, "password": password, "email": mail, "address": address, "phoneNumber": phoneNumber, "birthday": birthday}
             else:
                 message_type = ERROR_CODE
 
-            # Add code and message length according to the format
-            data = chr(message_type)
+            # convert to binary
+            data_bytes = (chr(message_type)).encode()
 
             len_of_data = len(json.dumps(message_data))
-            data += convert_to_byte_string(len_of_data)
+            data_bytes += convert_to_byte_string(len_of_data)
 
-            data += json.dumps(message_data)
+            data_bytes += json.dumps(message_data).encode()
 
             # Send message to server as string
-            server_sock.sendall(data.encode())
+            server_sock.sendall(data_bytes)
 
             # Receive response from server as json
-            response = server_sock.recv(1024).decode()
-            
+            response = server_sock.recv(1024).decode("latin-1")
+
             # first byte is the code that helps us determine the type of response
             server_data_code = response[0]
 
             # first 5 bytes are header from server, irrelevant in python demo client
             server_data = json.loads(response[5:])
-            
+
             if server_data[STATUS_KEY] == ERROR_CODE:
                 print(server_data[ERROR_MSG_KEY])
             elif server_data[STATUS_KEY] == SUCCESS_CODE:
