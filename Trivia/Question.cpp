c85ec6b53e2fdd33a2117f0d8ec510e7f3cfa015
Shelly Slavutin <shelly.slavutin@gmail.com>
2024-04-25 23:33:38 +0300

    finished question class with how I think it should be

--- Trivia/Question.cpp
+++ Trivia/Question.cpp
@@ -4,6 +4,32 @@ Question::Question()
 {
 }
 
+Question::Question(const string question, const string correct, const string incorecct[NUM_OF_INCORRECT])
+{
+	this->_question = question;
+	this->_correctAnswer = correct;
+
+	for (int i = 0; i < NUM_OF_INCORRECT; i++)
+	{
+		this->_possibleAnswers.push_back(incorecct[i]);
+	}
+}
+
 Question::~Question()
 {
+}
+
+string Question::getQuestion() const
+{
+	return this->_question;
+}
+
+std::vector<string> Question::getPossibleAnswers() const
+{
+	return this->_possibleAnswers;
+}
+
+std::string Question::getCorrentAnswer() const
+{
+	return this->_question;
 }
--- Trivia/Question.h
+++ Trivia/Question.h
@@ -4,18 +4,55 @@
 #include <vector>
 #include <string>
 
+using std::string;
+
+#define NUM_OF_INCORRECT 3
+
 class Question
 {
 public:
-	Question();
-	~Question();
+    /**
+     * Default constructor for the Question class.
+     */
+    Question();
 
-	std::string getQuestion() const;
-	std::vector<std::string> getPossibleAnswers() const;
-	std::string getCorrentAnswer() const;
-	unsigned int getCorrectAnswerId() const;
+    /**
+     * Constructor for the Question class with parameters.
+     *
+     * @param question The question text.
+     * @param correct The correct answer.
+     * @param incorecct An array containing incorrect answers.
+     */
+    Question(const string question, const string correct, const string incorecct[NUM_OF_INCORRECT]);
+
+    /**
+     * Destructor for the Question class.
+     */
+    ~Question();
+
+    /**
+     * Getter method to retrieve the question text.
+     *
+     * @return The question text.
+     */
+    string getQuestion() const;
+
+    /**
+     * Getter method to retrieve the possible answers.
+     *
+     * @return A vector containing the possible answers.
+     */
+    std::vector<string> getPossibleAnswers() const;
+
+    /**
+     * Getter method to retrieve the correct answer.
+     *
+     * @return The correct answer.
+     */
+    std::string getCorrentAnswer() const;
 
 private:
-	std::string _question;
-	std::vector<std::string> _possibleAnswers;
+    string _question;               // The question text
+    std::vector<string> _possibleAnswers;  // Vector containing possible answers
+    std::string _correctAnswer;     // The correct answer
 };
