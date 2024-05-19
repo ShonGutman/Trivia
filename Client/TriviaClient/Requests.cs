f114277719145756c11d9463ed0cd6c1ec47a585
Shon Gutman <gshon2007@gmail.com>
2024-05-19 15:51:07 +0300

    added support of more requests

--- Client/TriviaClient/Requests.cs
+++ Client/TriviaClient/Requests.cs
@@ -14,6 +14,12 @@ namespace TriviaClient
 	        LOGIN_REQUEST_ID = 1,
 	        SIGN_UP_REQUEST_ID = 2,
             LOGOUT_REQUEST_ID = 3,
+            JOIN_ROOM_REQUEST_ID = 4,
+            CREATE_ROOM_REQUEST_ID = 5,
+            GET_ALL_ROOMS_REQUEST_ID = 6,
+            GET_PLAYERS_IN_ROOM_REQUEST_ID = 7,
+            GET_PERSONAL_SCORE_REQUEST_ID = 8,
+            GET_HIGHEST_SCORE_REQUEST_ID = 9,
             ERROR_REQUEST_ID = 99
         }
         public class LoginRequest
@@ -45,6 +51,22 @@ namespace TriviaClient
                 this.address = address;
                 this.phoneNumber = phoneNumber;
                 this.birthday = birthday;
+            }
+        }
+
+        public class CreateRoomRequest
+        {
+            string roomName { get; set; }
+            uint playersNum { get; set; }
+            uint questionNum { get; set; }
+            uint timePerQuestion { get; set; }
+
+            public CreateRoomRequest(string roomName, uint playersNum, uint questionNum, uint timePerQuestion)
+            {
+                this.roomName = roomName;
+                this.playersNum = playersNum;
+                this.questionNum = questionNum;
+                this.timePerQuestion = timePerQuestion;
             }
         }
     }
