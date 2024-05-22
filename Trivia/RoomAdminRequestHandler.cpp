21bd3e598a33e596343c5faf6baf15b1403a1478
Shelly Slavutin <shelly.slavutin@gmail.com>
2024-05-22 14:08:51 +0300

    did closeRoom? not sure tho

--- Trivia/RoomAdminRequestHandler.cpp
+++ Trivia/RoomAdminRequestHandler.cpp
@@ -1,2 +1,62 @@
 #include "RoomAdminRequestHandler.h"
 
+bool RoomAdminRequestHandler::isRequestRelevant(const RequestInfo& info)
+{
+	return info.id == CLOSE_ROOM_REQUEST_ID || info.id == START_GAME_REQUEST_ID ||info.id == GET_ROOM_STATE_REQUEST_ID;
+}
+
+RequestResult RoomAdminRequestHandler::handleRequest(const RequestInfo& info, LoggedUser user)
+{
+	switch (info.id)
+	{
+	case START_GAME_REQUEST_ID:
+		return this->startGame(info);
+		break;
+	case CLOSE_ROOM_REQUEST_ID:
+		return this->closeRoom(info, user);
+		break;
+	case GET_ROOM_STATE_REQUEST_ID:
+		return this->getRoomState(info);
+		break;
+
+	default:
+		throw std::runtime_error("Illegal option!");
+	}
+}
+
+RequestResult RoomAdminRequestHandler::closeRoom(const RequestInfo& info, LoggedUser user)
+{
+	//RoomAdminRequest request = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
+
+	RequestResult result;
+
+	RoomManager& RoomManager = _factoryHandler.getRoomManager();
+
+	try
+	{
+		CloseRoomResponse response;
+		this->_roomManager.deleteRoom(this->_room.getRoomData().id);
+
+		//SUCCESS reponse to login
+		response.status = SUCCESS;
+
+		//assign to MenuHandler 
+		result.newHandler = _factoryHandler.createRoomAdminRequestHandler(user);
+		result.response = JsonResponsePacketSerializer::serializerResponse(response);
+	}
+	catch (const std::exception& e)
+	{
+		ErrorResponse response;
+
+		//FAILED reponse to login
+		response.message = e.what();
+		response.id = CLOSE_ROOM_RESPONSE_ID;
+
+		//assign to LoginHandler
+		result.newHandler = _factoryHandler.createRoomAdminRequestHandler(user);
+		result.response = JsonResponsePacketSerializer::serializerResponse(response);
+	}
+
+	return result;
+}
+
--- Trivia/RoomAdminRequestHandler.h
+++ Trivia/RoomAdminRequestHandler.h
@@ -3,6 +3,7 @@
 #include "Room.h"
 #include "RoomManager.h"
 #include "LoggedUser.h"
+#include "IRequestHandler.h"
 #include "RequestHandlerFactory.h"
 
 // to avoid circular definition
@@ -13,18 +14,19 @@ class RoomAdminRequestHandler : public IRequestHandler
 public:
 	RoomAdminRequestHandler(Room&, RoomManager*);
 
-	bool isRequestRelevant(const RequestInfo& r);
+	bool isRequestRelevant(const RequestInfo& info) override;
 
-	RequestResult handleRequest(const RequestInfo& r, LoggedUser user);
+	RequestResult handleRequest(const RequestInfo& info, LoggedUser user);
 
-	RequestResult closeRoom(const RequestInfo& r);
-
-	RequestResult startGame(const RequestInfo& r);
-
-	RequestResult getRoomState(const RequestInfo& r);
 private:
 	Room _room;
 	LoggedUser _user;
 	RoomManager& _roomManager;
 	RequestHandlerFactory& _factoryHandler;
+
+	RequestResult closeRoom(const RequestInfo& info, LoggedUser user);
+
+	RequestResult startGame(const RequestInfo& info);
+
+	RequestResult getRoomState(const RequestInfo& info);
 };
