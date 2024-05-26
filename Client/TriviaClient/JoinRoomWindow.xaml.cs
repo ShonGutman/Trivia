11a2df61913189e9aba768afcd0773637e642d4f
Shon Gutman <gshon2007@gmail.com>
2024-05-27 01:04:13 +0300

    added BackgroundWorker and refresh func to join room

--- Client/TriviaClient/JoinRoomWindow.xaml.cs
+++ Client/TriviaClient/JoinRoomWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,13 +23,19 @@ namespace TriviaClient
     {
         private Communicator communicator;
         private string username;
+        private BackgroundWorker background_worker = new BackgroundWorker();
         public JoinRoomWindow(Communicator communicator, string username)
         {
             this.communicator = communicator;
             this.username = username;
             InitializeComponent();
             UserLabel.Content = "Hello, " + username;
+
+            startRefreshTread();
+
         }
+
+
 
         private void RoomsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -39,9 +47,25 @@ namespace TriviaClient
 
         }
 
-        private void CreateRoom_Click(object sender, RoutedEventArgs e)
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        
+
+        public void startRefreshTread()
+        {
+            background_worker.DoWork += background_worker_DoWork;
+            background_worker.ProgressChanged += background_worker_ProgressChanged;
+            background_worker.RunWorkerCompleted += background_worker_RunWorkerCompleted;
+
+            background_worker.WorkerSupportsCancellation = true;
+            background_worker.WorkerReportsProgress = true;
+
+            if (!background_worker.IsBusy)
+            {
+                background_worker.RunWorkerAsync();
+            }
         }
     }
 }
