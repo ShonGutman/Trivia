0a2a58bef26fdda7a3105a641ba385086073ded1
Shon Gutman <gshon2007@gmail.com>
2024-06-02 14:57:05 +0300

    i think if am done with game

--- Trivia/Game.cpp
+++ Trivia/Game.cpp
@@ -2,15 +2,80 @@
 
 Question Game::getQuestionForUser(const LoggedUser& user) const
 {
-    return _players.at(user).currentQuestion;
+	for (auto& it : _players)
+	{
+		if (it.user == user && it.isStillPlaying)
+		{
+			return it.playerGameData.currentQuestion;
+		}
+	}
+
+	throw std::runtime_error("Can't get question for user " + user.getName());
+
 }
 
 void Game::removePlayer(const LoggedUser& user)
 {
-	bool erased = _players.erase(user);
-
-	if (!erased)
+	for (auto& it : _players)
 	{
-		throw std::runtime_error("User doesn't exist in game, can't erase ...");
+		if (it.user == user && it.isStillPlaying)
+		{
+			//make user not playing anymore
+			it.isStillPlaying = false;
+
+			//decrease amount of playing players
+			_numOfPlayersStillPlaying--;
+
+			return;
+		}
 	}
+
+	throw std::runtime_error("User doesn't exist in game, can't remove him!");
+}
+
+void Game::submitAnswer(const LoggedUser& user, const unsigned int answerID, const double timeForQuestion)
+{
+	for (auto& it : _players)
+	{
+		if (it.user == user && it.isStillPlaying)
+		{
+			bool isRight = it.playerGameData.currentQuestion.getCorrectAnserID() == answerID;
+
+			//add time it took to answer to avg
+			it.playerGameData.avgTimeForAns = addToAvg(it.playerGameData.avgTimeForAns,
+				it.playerGameData.numRightAns + it.playerGameData.numWrongAns, timeForQuestion);
+
+			//check if player was right or wrong
+			if (isRight)
+			{
+				it.playerGameData.numRightAns++;
+			}
+
+			else
+			{
+				it.playerGameData.numWrongAns++;
+			}
+
+			//increase question ID by one and check if player reached the limit of questions
+			if (++it.currentQuestionID == _questions.size())
+			{
+				it.isStillPlaying = false;
+				_numOfPlayersStillPlaying--;
+			}
+
+			//change the question to the player and increase the question ID
+			else
+			{
+				it.playerGameData.currentQuestion = _questions[it.currentQuestionID++];
+			}
+
+			return;
+		}
+	}
+		throw std::runtime_error("User isn't playing. can't submit answer");
+}
+
+double Game::addToAvg(const double avg, const unsigned int size, const double addedValue)
+{
+	return (avg * size + addedValue) / (size + 1);
 }
--- Trivia/Game.h
+++ Trivia/Game.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <set>
+#include <vector>
 #include <map>
 #include "Question.h"
 #include "LoggedUser.h"
@@ -11,6 +11,15 @@ struct GameData
 	unsigned int numRightAns = 0;
 	unsigned int numWrongAns = 0;
 	double avgTimeForAns = 0;
+
+};
+
+struct playerGame
+{
+	LoggedUser user;
+	GameData playerGameData;
+	unsigned int currentQuestionID = 0;
+	bool isStillPlaying = true;
 };
 
 class Game
@@ -18,28 +27,57 @@ class Game
 public:
 
 	/*
-	* Retrieves the current question for the specified user. It looks up the user in the
-	* players map and returns their current question.
+	* Retrieves the current question for the specified user if the user is still playing.
+	* It iterates through the players map to find the user and checks if they are still playing.
+	* If found, it returns the current question assigned to the user.
 	*
 	* @param user The logged-in user for whom the question is to be retrieved.
 	* @return The current question assigned to the user.
-	* @throws std::out_of_range If the user is not found in the players map.
+	* @throws std::runtime_error If the user is not found in the players map or is not still playing.
 	*/
 	Question getQuestionForUser(const LoggedUser& user) const;
 
 	/*
-	* Removes the specified user from the game. It attempts to erase the user from the
-	* players map. If the user does not exist in the game, an exception is thrown.
+	* Removes the specified user from the game by marking them as no longer playing.
+	* It iterates through the players map to find the user and checks if they are still playing.
+	* If found, it updates their status to not playing and decreases the count of players still playing.
+	* If the user does not exist or is not found, an exception is thrown.
 	*
 	* @param user The logged-in user to be removed from the game.
-	* @throws std::runtime_error If the user does not exist in the game.
+	* @throws std::runtime_error If the user does not exist in the game or is not found.
 	*/
 	void removePlayer(const LoggedUser& user);
+
+	/*
+	* Submits an answer for the specified user, updates their game data, and progresses to the next question.
+	* It iterates through the players map to find the user and checks if they are still playing.
+	* If found, it verifies if the submitted answer is correct, updates the average time for answering questions,
+	* and increments the count of right or wrong answers accordingly. It then either progresses to the next question
+	* or marks the user as no longer playing if they have reached the question limit.
+	* If the user is not found or is not playing, an exception is thrown.
+	*
+	* @param user The logged-in user submitting the answer.
+	* @param answerID The ID of the answer submitted by the user.
+	* @param timeForQuestion The time taken by the user to answer the question.
+	* @throws std::runtime_error If the user is not found or is not playing.
+	*/
+	void submitAnswer(const LoggedUser& user, const unsigned int answerID, const double timeForQuestion);
 
 
 private:
 
-	std::set<Question> _questions;
-	std::map<LoggedUser, GameData> _players;
-	unsigned int gameID;
+	/*
+	* Calculates the new average after adding a new value to a set of existing values.
+	*
+	* @param avg The current average of the existing values.
+	* @param size The number of existing values.
+	* @param addedValue The new value to be added to the set.
+	* @return The new average after adding the new value.
+	*/
+	double addToAvg(const double avg, const unsigned int size, const double addedValue);
+
+	std::vector<Question> _questions;
+	std::vector<playerGame> _players;
+	unsigned int _gameID;
+	unsigned int _numOfPlayersStillPlaying;
 };
