63fe219e3d26de19708c1893efe959bbf22b91da
Shelly Slavutin <shelly.slavutin@gmail.com>
2024-06-02 16:27:54 +0300

    finished game window

---
+++ Client/TriviaClient/GameWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ans1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ans2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ans3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ans4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EXIT_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
