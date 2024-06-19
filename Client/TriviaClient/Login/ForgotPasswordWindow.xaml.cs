6ea4e71ffd1c3945477b551ff596bb477e676e45
Shelly Slavutin <shelly.slavutin@gmail.com>
2024-06-19 19:43:12 +0300

    basic functionality of the forget password

--- Client/TriviaClient/Login/ForgotPasswordWindow.xaml.cs
+++ Client/TriviaClient/Login/ForgotPasswordWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace TriviaClient.Login
 {
@@ -20,25 +9,47 @@ namespace TriviaClient.Login
     public partial class ForgotPasswordWindow : Window
     {
         private Communicator communicator;
+        private string sentCode;
 
         public ForgotPasswordWindow(Communicator communicator)
         {
             InitializeComponent();
-        }
-
-        private void ConfirmCode_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void backButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            this.communicator = communicator;
         }
 
         private void SendCodeButton_Click(object sender, RoutedEventArgs e)
         {
+            string email = EmailTextBox.Text;
+            if (string.IsNullOrEmpty(email))
+            {
+                ErrorPopup errorWindow = new ErrorPopup("Please enter email");
+                errorWindow.ShowDialog();
+                return;
+            }
 
+            // TODO : SEND THE AUTH CODE
+        }
+
+        private void ConfirmCode_Click(object sender, RoutedEventArgs e)
+        {
+            string enteredCode = AuthCodeTextBox.Text;
+            if (enteredCode == sentCode)
+            {
+                //TODO
+            }
+            else
+            {
+                ErrorPopup errorWindow = new ErrorPopup("Incorrect authentication code. Please try again.");
+                errorWindow.ShowDialog();
+                return;
+            }
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow(communicator);
+            this.Close();
+            loginWindow.Show();
         }
     }
 }
